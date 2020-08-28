53ea6a5e27799d174f6608383065c7a00978916e
Murao <yoshinobu612@icloud.com>
2020-08-28 09:51:29 +0900

    first commit

---
+++ HobbyRecord WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  HobbyRecord WatchKit Extension
+//
+//  Created by 村尾慶伸 on 2020/08/28.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
