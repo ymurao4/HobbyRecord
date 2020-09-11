60fec8c66435a833e1612d7fb01b3d142e199c0f
Murao <yoshinobu612@icloud.com>
2020-09-11 11:28:34 +0900

    before remove watchos

--- HobbyRecord WatchKit Extension/ContentView.swift
+++ HobbyRecord WatchKit Extension/ContentView.swift
@@ -11,7 +11,14 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello, World!")
+
+        List {
+
+            ForEach(0..<5, id: \.self) { i in
+
+                Text("\(i + 1)番目だお")
+            }
+        }
     }
 }
 
